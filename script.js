48c068ef29feb48aeb682ae3bcea929e22f2f812
rachel2bester <rachel2bester@gmail.com>
2022-05-25 16:40:29 +0200

    tried to fix numberChange

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ import pokemonArray from "./data/pokemon.js";
 const cardContainer = document.querySelector(".card-container");
 const body = document.querySelector("body");
 
-const search = document.querySelector("#search");
-const searchBy = document.querySelector("#search-by");
-const numOfResults = document.querySelector("#num-of-results");
+
+
 
 
 const getCardHTML = (pokemon) => {
@@ -22,8 +21,8 @@ const getCardHTML = (pokemon) => {
 }
 
 const onSearchChange = (event) => {
-
-    pokemons.forEach((pokemon) => {
+    
+    pokemons.forEach((pokemon, index) => {
         let attribute = null;
         if (searchBy.options[searchBy.selectedIndex].text === "Name") {
             attribute = pokemon.obj.name;
@@ -39,17 +38,51 @@ const onSearchChange = (event) => {
             pokemon.htmlCard.style.display = "none";
         }
     })
+
 }
 
-
-
-
-//insert form after h1
-
-//body.innerHTML
+const onNumberChange = (event) => {
+    pokemons.forEach((pokemon, index) => {
+        if (index < numOfResults) {
+            pokemon.htmlCard.style.display = "";
+        } else {
+            pokemon.htmlCard.style.display = "none";
+        }
+    });
+}
 
 //generate cards
 pokemonArray.forEach((pokemon) => cardContainer.innerHTML += getCardHTML(pokemon));
+
+//add form
+const formHTML = `
+    <form>
+        <label for="search">Search</label>
+        <input type="text" id="search">
+        <label for="search-by">Search by</label>
+        <select id="search-by">
+            <option value="name">Name</option>
+            <option value="id">ID Number</option>
+            <option value="type">Type</option>
+        </select>
+        
+        <label for="num-of-results" value=10>Number of Results to Display</label>
+        <input type="number" id="num-of-results">
+    </form>
+`
+{/* <label for="sort">Sort by</label>
+<select id="sort">
+    <option value="name">Name</option>
+    <option value="id">ID Number</option>
+</select> */}
+
+const indexOfMain = body.innerHTML.indexOf('<main');
+console.log(indexOfMain);
+body.innerHTML = body.innerHTML.substring(0, indexOfMain) + formHTML + body.innerHTML.substring(indexOfMain, body.innerHTML.length)
+
+const search = document.querySelector("#search");
+const searchBy = document.querySelector("#search-by");
+const numOfResults = document.querySelector("#num-of-results");
 
 const pokemonCards = document.querySelectorAll(".card");
 const pokemons = [];
@@ -65,6 +98,8 @@ pokemonArray.forEach((pokemon, index) => {
 pokemons.forEach((pokemon) => pokemon.htmlCard.style.display = "");
 
 search.addEventListener("input", onSearchChange);
+
+//numOfResults.addEventListener("input", onNumberChange);
 
 
 
