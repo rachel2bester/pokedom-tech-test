db1cff70fc32d7b9b95f0c05e33c67c291137f49
rachel2bester <rachel2bester@gmail.com>
2022-05-25 12:55:44 +0200

    made getCardHTML to generate cards

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+import pokemonArray from "./data/pokemon.js";
+
 const cardContainer = document.querySelector(".card-container");
 
-const addCard = (container, pokemon) => {
-    container.innerHTML += `
+const getCardHTML = (pokemon) => {
+    const capitalisedName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    const cardHTML = `
         <div class="card">
-            <div class="card__image" src="${pokemon.sprite}" alt="${pokemon.name}"></div>
+            <img class="card__image" src="${pokemon.sprite}" alt="${pokemon.name}"/>
             <div class="card__content">
-                <p class="card__heading">${pokemon.name}</p>
-                <p class="card__text">${pokemon.name} (#${pokemon.id}) is a type ${pokemon.type.join(" & ")} pokemon</p>
+                <p class="card__heading">${capitalisedName}</p>
+                <p class="card__text">${capitalisedName} (#${pokemon.id}) is a type ${pokemon.types.join(" & ")} pokemon</p>
             </div>
         </div>
     `;
+    return cardHTML;
 }
+
+pokemonArray.forEach((pokemon) => {
+    cardContainer.innerHTML += getCardHTML(pokemon);
+});
 
